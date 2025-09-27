50190188167473d75880d707f85614e7b02734c6
scm0809 <scm0809@naver.com>
2025-09-27 16:26:58 +0900

    Title: 남녀노소 구분짓기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250927/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,12 @@
+gender=int(input())
+age=int(input())
+if gender == 0:
+    if age >=19:
+        print("MAN")
+    else :
+        print("BOY")
+if gender == 1:
+    if age>=19 :
+        print("WOMAN")
+    else :
+        print("GIRL")
