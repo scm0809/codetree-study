b5a934328bf9b262e7065df5a7b30a874858aa9c
scm0809 <scm0809@naver.com>
2025-09-27 16:42:51 +0900

    Title: 윤년인가, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250927/윤년인가/is-leap-year.py
+++ 250927/윤년인가/is-leap-year.py
@@ -1,6 +1,11 @@
 Y=int(input())
 if Y%4==0:
-    if Y%100==0 and Y%400!=0:
-        print("false")
+    if Y%100==0 :
+        if Y%400!=0:
+            print("false")
+        else:
+            print("true")
     else:
         print("true")
+else:
+    print("false")
